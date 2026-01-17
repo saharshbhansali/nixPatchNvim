2d428f7d17272bd57c0fe78b66ec0f382e59937a
saharshbhansali <saharsh.bhansali@gmail.com>
2026-01-18 03:46:50 +0530

    feat: lazyvim multicursor (vim-visual-multi)

---
+++ lua/plugins/multicursor.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+    lazy = false, -- Set to false to ensure it loads correctly with LazyVim
+    config = function()
+      -- -- Optional: Add your custom configuration options here
+      -- vim.g.VM_maps = {
+      --   ["<C-d>"] = "Add Line Down", -- Example custom keymap
+      -- }
+    end,
+  },
+}
+
